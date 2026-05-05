e8af6813cdc476d14f1d7a60a8688340347f664f
Joshua Karanja <jokaranja26@gmail.com>
2026-05-05 22:32:41 +0300

    repaired Navbar styling to accomodate reacticon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,35 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import { BiLogoReact } from 'react-icons/bi'
+import React from "react";
+import { Link } from "react-router-dom";
+import { BiLogoReact } from "react-icons/bi";
+import "./Navbar.css";
+
 function Navbar() {
   return (
-    <nav className="bg-slate-800 shadow-lg flex items-center justify-around py-3 px-32 fixed top-0 left-0 w-full">
-        <Link to="/">
-        <span className='font-semibold text-lg flex items-center gap-3 text-blue-400'>
-            <BiLogoReact className='text-6xl'/>
-            <span>React Router</span>
-        </span>
+    <nav className="navbar">
+      <Link to="/" className="navbar-brand">
+        <BiLogoReact />
+        <span>React Router</span>
+      </Link>
+
+      <div className="navbar-links">
+        <Link to="/" className="navbar-link">
+          Home
         </Link>
 
-        <div className='flex items-center gap-5 text-black'>
-            <Link to="/" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300">
-            Home
-        
+        <Link to="/about" className="navbar-link">
+          About
         </Link>
 
-        <Link to="/about" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300">
-            About
-        
-        </Link>
-        <Link to="/contact" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300">
-            Contact
-        </Link>
-        <Link to="/services" className="py-1 px-3 text-lg font-light text-white hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300">
-            services
-        
+        <Link to="/contact" className="navbar-link">
+          Contact
         </Link>
 
-        </div>
-
-
+        <Link to="/services" className="navbar-link">
+          Services
+        </Link>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
